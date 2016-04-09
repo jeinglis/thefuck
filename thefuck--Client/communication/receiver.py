f4f6c6c2eacb7ddcbfd7df6959ba23f4856bdd1c
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 17:43:20 -0600

    now uses pickle

--- thefuck--Client/communication/receiver.py
+++ thefuck--Client/communication/receiver.py
@@ -1,16 +1,17 @@
 from socket import *
+import pickle
 from .serverFacade import ServerFacade
 
 class Reciever():
-    def receive_from_server():
-      messageCount = 0
-     commandList = []
+    def receive_from_server(clientSocket):
+        messageCount = 0
+        commandList = []
 
         while True:
             if messageCount ==0:
-                messageCount = clientSocket.recv(1024)
+                messageCount = clientSocket.recv(4)
           else:
-              corrected = clientSocket.recv(1024)
+              corrected = pickle.loads(clientSocket.recv(1024))
               commandList.append(corrected)
                if commandList.length == messageCount:
                   returnCorrected(commandList)
