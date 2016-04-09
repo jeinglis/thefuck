59e23455f7acf0172d7abc19f91b08763efed26a
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 13:35:51 -0600

    implemented server receiever

---
+++ thefuck--Server/communication/receiver.py
@@ -0,0 +1,12 @@
+from socket import *
+
+serverPort - 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print 'The Server is ready to receiver'
+while 1:
+        connectionSocket, addr = serverSocket.accept()
+        command - connectionSocket.recv(1024)
+        receiveCommand(command, connectionSocket)
+        connectionSocket.close()
