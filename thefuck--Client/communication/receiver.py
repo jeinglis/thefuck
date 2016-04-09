356032cc464657726a2078194167ab00335d6481
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 17:20:49 -0600

    fixed spacing

--- thefuck--Client/communication/receiver.py
+++ thefuck--Client/communication/receiver.py
@@ -2,7 +2,6 @@ from socket import *
 from .serverFacade import ServerFacade
 
 class Reciever():
-
     def receive_from_server():
       messageCount = 0
      commandList = []
@@ -13,10 +12,10 @@ class Reciever():
           else:
               corrected = clientSocket.recv(1024)
               commandList.append(corrected)
-               if commandList.length == messageCount
+               if commandList.length == messageCount:
                   returnCorrected(commandList)
                   clientSocket.close()
                   break
 
     def return_corrected(correctedList):
-     ServerFacade.receive_corrected(correctedList)
+        ServerFacade.receive_corrected(correctedList)
