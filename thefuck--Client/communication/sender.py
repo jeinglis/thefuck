4011ac2279536ea846cec84bf97298bf270f379f
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 17:26:09 -0600

    hardcoded server ip

--- thefuck--Client/communication/sender.py
+++ thefuck--Client/communication/sender.py
@@ -2,12 +2,12 @@ from socket import *
 from .receiver import Receiver
 
 class Sender():
-    serverName = 'theFuckServer'
-    serverPort = 9001
+    server_IP = 246.162.52.23
+    server_Port = 9001
 
     def sendCommand(toBeCorrected):
-         clientSocket = socket(AF_INT, SOCK_STREAM)
-        clientSocket.connect((serverName,serverPort))
+        clientSocket = socket(AF_INT, SOCK_STREAM)
+        clientSocket.connect((server_IP, serverPort))
         clientSocket.send(toBeCorrected)
         Receiver.receive_from_server()
 
