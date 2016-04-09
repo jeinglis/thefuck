9328152849f3b1943fcba38d7b68538fe092dc45
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 13:04:40 -0600

    add listening loop to receiver

--- thefuck--Client/communication/receiver.py
+++ thefuck--Client/communication/receiver.py
@@ -4,11 +4,19 @@ from .serverFacade import ServerFacade
 class Reciever():
 
 def receive_from_server():
+    messageCount = 0
+    commandList = []
 
-    corrected = clientSocket.recv(1024)
-    returnCorrected(corrected)
-    clientSocket.close()
+    while True:
+        if messageCount ==0:
+            messageCount = clientSocket.recv(1024)
+        else:
+            corrected = clientSocket.recv(1024)
+            commandList.append(corrected)
+            if commandList.length == messageCount
+                returnCorrected(commandList)
+                clientSocket.close()
+                break
 
-def return_corrected(corrected):
-
-    ServerFacade.receive_corrected(corrected)
+def return_corrected(correctedList):
+    ServerFacade.receive_corrected(correctedList)
