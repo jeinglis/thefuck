1e8fed41eaf05ee6376e7e3f0dbdd0fa606e4b6d
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 17:49:29 -0600

    removed changes to sender

--- thefuck--Client/communication/sender.py
+++ thefuck--Client/communication/sender.py
@@ -1,15 +1,4 @@
-from socket import *
-from .receiver import Receiver
 
-class Sender():
-    serverName = 'theFuckServer'
-    serverPort = 9001
-
-    def sendCommand(toBeCorrected):
-         clientSocket = socket(AF_INT, SOCK_STREAM)
-        clientSocket.connect((serverName,serverPort))
-        clientSocket.send(toBeCorrected)
-        Receiver.receive_from_server()
 
 
 
