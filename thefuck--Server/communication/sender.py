346412ba17da947b8005052b9ddfa2010b659dfe
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 13:59:19 -0600

    formatting

--- thefuck--Server/communication/sender.py
+++ thefuck--Server/communication/sender.py
@@ -1,10 +1,9 @@
 from socket import *
 
 class ServerSender():
-    
-def sendToClient(connectionSocket, correctedList):
-    connectionSocket.send(len(correctedList))
-    for items in correctedList
-        connectionSocket.send(items)
-    connectionSocket.close()
+    def sendToClient(connectionSocket, correctedCommands):
+        connectionSocket.send(len(correctedCommands))
+        for items in correctedCommands
+          connectionSocket.send(items)
+          connectionSocket.close()
     
