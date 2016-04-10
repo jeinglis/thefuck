d0f579d7f194c6722d5110230277b7070c7147f9
James Inglis <jeinglis@ucalgary.ca>
2016-04-09 18:56:58 -0600

    now that it's serialized I can get the whole list at once

--- thefuck--Client/communication/receiver.py
+++ thefuck--Client/communication/receiver.py
@@ -4,19 +4,23 @@ from .serverFacade import ServerFacade
 
 class Reciever():
     def receive_from_server(clientSocket):
-        messageCount = 0
-        commandList = []
+        # messageCount = 0
+        # commandList = []
 
-        while True:
-            if messageCount ==0:
-                messageCount = clientSocket.recv(4)
-          else:
-              corrected = pickle.loads(clientSocket.recv(1024))
-              commandList.append(corrected)
-               if commandList.length == messageCount:
-                  returnCorrected(commandList)
-                  clientSocket.close()
-                  break
+        # while True:
+        #     if messageCount ==0:
+        #         messageCount = clientSocket.recv(4)
+        #   else:
+        #       corrected = pickle.loads(clientSocket.recv(1024))
+        #       commandList.append(corrected)
+        #        if commandList.length == messageCount:
+        #           returnCorrected(commandList)
+        #           clientSocket.close()
+        #           break
+
+        corrected = pickle.loads(clientSocket.recv(1024))
+        clientSocket.close()
+        returnCorrected(corrected)
 
     def return_corrected(correctedList):
         ServerFacade.receive_corrected(correctedList)
